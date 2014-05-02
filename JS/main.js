398bc13d979c2f123b7b9d187e5f5fc5507cc4ca
magnumthatyrant <max@liquorice.co.za>
2014-05-02 11:47:56 +0200

    required change in CSS for text to display properly, corrected

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,18 @@
 /*
  * Author : Max-Andre Zil
  * Version: 0.21
- * Date:
- * */
-
+ * Date: 29.04.2014
+ * Functions: numToScreen() - displays the number pressed in the appropriate text field
+ *            signPressed() - displays the sign pressed in the appropriate text box and changes the value
+ *                            of sign_used.
+ *  */
+/* GLOBAL VARIABLES */
 var sign_used = 0;
+/* GLOBAL VARIABLES */
 
+//numToScreen - displays the number pressed in the appropriate text box based on whether or not a sign button has been pressed.
 function numToScreen( num ){
-    if( sign_used === 0 ){
+    if( sign_used == 0 ){
         first_num.value += num;
     }else{
         second_num.value += num;
